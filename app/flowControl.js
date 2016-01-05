9c6e89f7889b1c7043d3ab90c3236880e79c8aa7
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2016-01-04 23:20:28 -0500

    add responses fpr bestPractices, flowContro;, logicalOperators

--- app/flowControl.js
+++ app/flowControl.js
@@ -13,14 +13,21 @@ exports.flowControlAnswers = {
 
     if (num % 15 === 0) {
       return 'fizzbuzz';
-    } else if (num % 3 === 0) {
+    } 
+
+    if (num % 3 === 0) {
       return 'fizz';
-    } else if (num % 5 === 0) {
+    } 
+
+    if (num % 5 === 0) {
       return 'buzz'
-    } else if (isNAN(num) || num !== num) {
-      return false;
-    } else {
-      return num;
     }
+
+    if (typeof num !== 'number') {
+      return false;
+    } 
+
+    return num;
+    
   } 
 };
